cd50d182c1a8e5a692b5f5f31964f870e7996e14
DrMvF <mvf@ikigami.de>
2025-06-23 22:36:10 +0000

    Cleaned up DB credentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,14 @@
 import type { Config } from "drizzle-kit";
-
 export default {
   schema: "./drizzle/schema.ts",
   out: "./drizzle/migrations",
   dialect: "mysql",
   dbCredentials: {
-    host: process.env.DATABASE_HOST ?? "aws.connect.psdb.cloud",
+    host: process.env.DATABASE_HOST!,
     port: 3306,
-    user: process.env.DATABASE_USER ?? "722l94iuw42ah7wt7w4d",
-    password: process.env.DATABASE_PASSWORD ?? "pscale_pw_6SYhkT02Uuhek9mVtHEvIG8q51PMkVBcFEXs7GcmM6M",
-    database: process.env.DATABASE_NAME ?? "ikigaistrategist",
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    database: process.env.DATABASE_NAME!,
     ssl: {
       rejectUnauthorized: true,
     },
