3144486d114dd0277f898af16316989fa17f5bed
DrMvF <mvf@ikigami.de>
2025-07-13 21:50:56 +0000

    Refine sidebar layout and formatting adjustments

--- app/(flowlayout)/onboarding/page.tsx
+++ app/(flowlayout)/onboarding/page.tsx
@@ -60,13 +60,13 @@ export default function OnboardingPage() {
         <p className="text-xl sm:text-2xl mb-6">
           Your purpose isn’t broken. <br /> Your rhythm is just ignored.
         </p>
-        <p className="text-base sm:text-2xl mb-4">
+        <p className="text-base sm:text-xl mb-4">
           Let’s begin by honoring your body. <br />
           Tell us where you are in your cycle – <br />
           and we’ll align your prompts to your current phase.
         </p>
 
-        <label className="block text-xl sm:text-2xl mb-3 mt-10" htmlFor="cycleDay">
+        <label className="block text-xl sm:text-xl mb-3 mt-10" htmlFor="cycleDay">
           On which day of your menstrual cycle are you today?
         </label>
 
